fab67fe381b400f52651c27b76c35f0c9b2b273f
dm1tro69 <dimolg22@gmail.com>
2020-04-07 20:05:24 +0300

    last commit

--- ebooks/api/views.py
+++ ebooks/api/views.py
@@ -1,34 +1,46 @@
 from rest_framework import generics
 from rest_framework.generics import get_object_or_404
-
+from rest_framework import permissions
 from ebooks.models import Ebook, Review
 from ebooks.api.serializers import EbookSerializer, ReviewSerializer
+from .permissions import IsAdminUserOrReadOnly, IsReviewAuthorOrReadOnly
+from rest_framework.exceptions import ValidationError
+from .pagination import SmallSetPagination
 
 
 class EbookListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Ebook.objects.all()
+    queryset = Ebook.objects.all().order_by('id')
     serializer_class = EbookSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
+    pagination_class = SmallSetPagination
 
 
 class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ebook.objects.all()
     serializer_class = EbookSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
 
 from  rest_framework import mixins
 
 class ReviewListCreateAPIView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         ebook_pk = self.kwargs.get('ebook_pk')
         ebook = get_object_or_404(Ebook, pk=ebook_pk)
-        serializer.save(ebook=ebook)
+        review_author = self.request.user
+        review_queryset = Review.objects.filter(ebook=ebook, review_author=review_author)
+        if review_queryset.exists():
+            raise ValidationError('You Have Already Reviewed this Ebook')
+        serializer.save(ebook=ebook, review_author=review_author)
 
 
 class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsReviewAuthorOrReadOnly]
 
 
 # class EbookListCreateAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
